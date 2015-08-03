e5767cad6b9f5b73b83cb1b217b9414a3a5d3c6d
Fabse92 <jfs92@yahoo.de>
2015-08-03 15:55:02 +0200

    parallelisierung um beste Parameter zu finden

--- sys-call-overhead/predicting_with_unification.R
+++ sys-call-overhead/predicting_with_unification.R
@@ -44,32 +44,64 @@ normalize <- function(x,min = NULL,max = NULL){
   (x - min) / (max - min)
 }
 
-d.norm <- data.frame(apply(d.unified[c("OpTyp","DeltaOffset", "Size","Quantile_0.1","Quantile_0.5", "Quantile_0.9")],2,normalize))
-#d.norm$Quantile_0.1 = normalize(d.unified$Quantile_0.1, min(d.unified$min_Duration), max(d.unified$max_Duration)) 
-#d.norm$Quantile_0.5 = normalize(d.unified$Quantile_0.5, min(d.unified$min_Duration), max(d.unified$max_Duration)) 
-#d.norm$Quantile_0.9 = normalize(d.unified$Quantile_0.9, min(d.unified$min_Duration), max(d.unified$max_Duration)) 
+shortest_duration = min(d.unified$min_Duration)
+longest_duration = max(d.unified$max_Duration)
 
-# Einsatz des Neuronalen Netzes
-net = neuralnet(Quantile_0.1+Quantile_0.5+Quantile_0.9~OpTyp+DeltaOffset+Size, d.norm, hidden = c(9))
-plot(net, rep = "best")
+d.norm <- data.frame(apply(d.unified[c("OpTyp","DeltaOffset", "Size")],2,normalize))
+#d.norm <- data.frame(apply(d.unified[c("OpTyp","DeltaOffset", "Size","Quantile_0.1","Quantile_0.5", "Quantile_0.9")],2,normalize))
+d.norm$Quantile_0.1 = normalize(d.unified$Quantile_0.1, shortest_duration, longest_duration) 
+d.norm$Quantile_0.5 = normalize(d.unified$Quantile_0.5, shortest_duration, longest_duration) 
+d.norm$Quantile_0.9 = normalize(d.unified$Quantile_0.9, shortest_duration, longest_duration) 
 
 # Test des Netzes auf dem Originalen Datenset
-temp_test <- data.frame(apply(d[c("OpTyp","DeltaOffset", "Size")],2,normalize))
+test_data <- data.frame(apply(d[c("OpTyp","DeltaOffset", "Size")],2,normalize))
+test_count = 100000
+temp_test = head(test_data, test_count)
 
-results = data.frame(compute(net, temp_test)$net.result)
-
-d.results = data.frame(Duration = d$Duration, pred_Quantile_0.1 = results$X1, pred_Quantile_0.5 = results$X2, pred_Quantile_0.9 = results$X3)
-
-# Resklaierung des Ergebnisses
-d.results$pred_Quantile_0.1 = d.results$pred_Quantile_0.1 * (max(d.unified$Quantile_0.1) - min(d.unified$Quantile_0.1)) + min(d.unified$Quantile_0.1)
-d.results$pred_Quantile_0.5 = d.results$pred_Quantile_0.5 * (max(d.unified$Quantile_0.5) - min(d.unified$Quantile_0.5)) + min(d.unified$Quantile_0.5)
-d.results$pred_Quantile_0.9 = d.results$pred_Quantile_0.9 * (max(d.unified$Quantile_0.9) - min(d.unified$Quantile_0.9)) + min(d.unified$Quantile_0.9)
-
-d.results$error= d.results$pred_Quantile_0.5 - d.results$Duration
-d.results$percent_error = (d.results$error / d.results$Duration)*100
-d.results$abspercent_error = abs(d.results$percent_error)
-
-mean(d.results$abspercent_error)
+parameter_test = data.frame(1,2,3)
+names(parameter_test) = c("layers,neurons,repetitions,threshold","mean_error","best_error")
+idx = 1
+nof_iterations = 1
+for (nof_layers in 2:2) {
+  for (nof_neurons in 4:4) {
+    layers = c(nof_neurons)
+    for (idx_layer in 2:nof_layers) {
+      layers = c(layers, nof_neurons)
+    }
+    for (nof_reps in 1:1) {
+      for (threshold_exponent in 3:5) { # threshold des Algorithmus ist 10^(-threshold_exponent); über 5 selten konvergent
+        test_threshold = 10^(-threshold_exponent)
+        mean_error = 0
+        best_error = 10000
+        for (count in 1:nof_iterations) {
+          # Einsatz des Neuronalen Netzes
+          net = neuralnet(Quantile_0.1+Quantile_0.5+Quantile_0.9~OpTyp+DeltaOffset+Size, d.norm, hidden = layers, threshold = test_threshold, algorithm = "rprop+", rep = nof_reps)
+          
+          results = data.frame(compute(net, temp_test)$net.result)
+          d.results = data.frame(Duration = head(d$Duration, test_count), pred_Quantile_0.1 = results$X1, pred_Quantile_0.5 = results$X2, pred_Quantile_0.9 = results$X3)
+          
+          # Resklaierung des Ergebnisses
+          d.results$pred_Quantile_0.1 = d.results$pred_Quantile_0.1 * (longest_duration - shortest_duration) + shortest_duration
+          d.results$pred_Quantile_0.5 = d.results$pred_Quantile_0.5 * (longest_duration - shortest_duration) + shortest_duration
+          d.results$pred_Quantile_0.9 = d.results$pred_Quantile_0.9 * (longest_duration - shortest_duration) + shortest_duration
+          
+          d.results$error= d.results$pred_Quantile_0.5 - d.results$Duration
+          d.results$percent_error = (d.results$error / d.results$Duration)*100
+          d.results$abspercent_error = abs(d.results$percent_error)
+          
+          mean_error = mean_error + mean(d.results$abspercent_error)
+          if (mean(d.results$abspercent_error) < best_error) {
+            best_error = mean(d.results$abspercent_error)
+          }
+        }
+        mean_error = mean_error / nof_iterations
+        test_type = c(nof_layers,nof_neurons,nof_reps,test_threshold)
+        parameter_test[idx,] = c(toString(test_type),mean_error, best_error)
+        idx = idx + 1
+      }
+    }
+  }
+}
 
 # Plot der echten Duration gegenüber der vorhergesagten
 plot(d.results$Duration, col="blue", ylim=c(-0.0001, 0.0004))
